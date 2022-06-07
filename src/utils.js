bfac4434c5bae88e4bf151687a7b6a6b8d73db8e
vikingviolinist <misopetrik@gmail.com>
2022-06-07 09:20:24 +0200

    🔨 Add utils

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+const convertMillisToMins = (millis) => Math.floor(millis / 1000 / 60);
+
+const formatTimeSlot = (start, end) => {
+  const padHours = (date) => String(new Date(date).getHours()).padStart(2, '0');
+  const padMins = (date) => String(new Date(date).getMinutes()).padStart(2, '0');
+  const startHours = padHours(start);
+  const startMins = padMins(start);
+  const endHours = padHours(end);
+  const endMins = padMins(end);
+  return `${startHours}:${startMins}-${endHours}:${endMins}`;
+};
+
+const getCurrentTimeInMillis = () => new Date().getTime();
+
+const getDateStartInMillis = (date) => new Date(date).setHours(0, 0, 0, 0);
+
+const getTimeInMillis = (date) => new Date(date).getTime();
+
+export { convertMillisToMins, formatTimeSlot, getCurrentTimeInMillis, getDateStartInMillis, getTimeInMillis };
